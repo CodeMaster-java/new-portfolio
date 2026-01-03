28daffc6eddc8179d7676483b8f3ae226a6bcff5
CodeMaster-java <robsonjosecorreacarvalho@github.com>
2026-01-03 15:03:49 -0300

    feat: adicionar suporte ao Vercel Analytics no portfólio

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 
 import { ThemeProvider } from '@/hooks/useThemeMode'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider>
       <App />
+      <Analytics />
     </ThemeProvider>
   </StrictMode>,
 )
